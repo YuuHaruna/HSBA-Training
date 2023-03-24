c6e422b171fbfa1a608248246fefdef689d457b8
Dawn Nguyen <lophocpzo@gmail.com>
2023-03-24 14:31:23 +0700

    Hide status bar

--- app/src/main/java/com/beetech/hsba/MainActivity.kt
+++ app/src/main/java/com/beetech/hsba/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.beetech.hsba
 
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.beetech.hsba.base.BaseActivity
 import com.beetech.hsba.ui.SplashFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,11 +20,25 @@ class MainActivity : BaseActivity() {
     override val layoutId: Int
         get() = R.id.container
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
+        super.onCreate(savedInstanceState)
+    }
+
     override fun initListener() {
     }
 
     override fun initView() {
         getViewController().addFragment(SplashFragment::class.java)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView.rootView).let {
+            it.hide(WindowInsetsCompat.Type.systemBars())
+            it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 
     override fun initData() {
