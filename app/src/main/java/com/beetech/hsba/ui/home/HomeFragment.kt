d1fa6d85273df0cbaf7b1f83be0f8370e9a7e2b5
Dawn Nguyen <lophocpzo@gmail.com>
2023-03-27 09:00:56 +0700

    Add login with Facebook and Google

--- app/src/main/java/com/beetech/hsba/ui/home/HomeFragment.kt
+++ app/src/main/java/com/beetech/hsba/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.beetech.hsba.ui.home
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.Html
@@ -24,6 +25,15 @@ import com.beetech.hsba.entity.SSOPlatform
 import com.beetech.hsba.extension.toast
 import com.beetech.hsba.utils.AsteriskPasswordTransformationMethod
 import com.beetech.hsba.utils.showHidePass
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,33 +45,25 @@ class HomeFragment : BaseFragment() {
 
     private val binding get() = _binding!!
 
-//    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var googleSignInClient: GoogleSignInClient
 
-//    private lateinit var googleLoginForResult: ActivityResultLauncher<Intent>
+    private lateinit var googleLoginForResult: ActivityResultLauncher<Intent>
 
-//    private lateinit var callbackManager: CallbackManager
+    private lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        googleLoginForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//            if (it.data != null) {
-//                try {
-//                    val acc = GoogleSignIn.getSignedInAccountFromIntent(it.data).getResult(ApiException::class.java)
-//                    Toast.makeText(
-//                        this,
-//                        "Login with Google success by account ${acc.email}",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                } catch (e: ApiException) {
-//                    Log.d("Login", "signInResult:failed code=" + e.statusCode)
-//                    Toast.makeText(
-//                        this,
-//                        "Login with Google fail: ${e.statusCode}",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            }
-//        }
+        googleLoginForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.data != null) {
+                try {
+                    val acc = GoogleSignIn.getSignedInAccountFromIntent(it.data).getResult(ApiException::class.java)
+                    toast("Login with Google success by account ${acc.email} and result code is ${it.resultCode}")
+                } catch (e: ApiException) {
+                    Log.d("Login", "signInResult:failed code=" + e.statusCode)
+                    if (it.resultCode != Activity.RESULT_CANCELED) toast("Login with Google fail: ${e.statusCode} and result code is ${it.resultCode}")
+                }
+            }
+        }
     }
 
     override fun onCreateView(
@@ -87,11 +89,9 @@ class HomeFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
 
-//        initGoogleSSO()
-//
-//        initFacebookSSO()
+        initGoogleSSO()
 
-//        binding.textViewLoginRegister.text = Html.fromHtml("")
+        initFacebookSSO()
 
         val ssoData = listOf(
             SSOEntity(R.drawable.zalo_logo, "Zalo", SSOPlatform.Zalo),
@@ -103,16 +103,16 @@ class HomeFragment : BaseFragment() {
         binding.recyclerViewLoginSso.apply {
             adapter = SSORecyclerViewAdapter(ssoData, object : OnClickSSO {
                 override fun loginSSO(type: SSOPlatform) {
-//                    when (type) {
-//                        SSOPlatform.Google -> googleSignIn()
-//
-//                        SSOPlatform.Facebook -> facebookSignIn()
-//
-//                        else -> {
-//                            googleSignInClient.signOut()
-//                            googleSignInClient.revokeAccess()
-//                        }
-//                    }
+                    when (type) {
+                        SSOPlatform.Google -> googleSignIn()
+
+                        SSOPlatform.Facebook -> facebookSignIn()
+
+                        else -> {
+                            googleSignInClient.signOut()
+                            googleSignInClient.revokeAccess()
+                        }
+                    }
                 }
             })
             layoutManager = LinearLayoutManager(requireContext())
@@ -131,55 +131,48 @@ class HomeFragment : BaseFragment() {
         }
     }
 
-//    private fun initFacebookSSO() {
-//        callbackManager = create()
-//
-//        LoginManager.getInstance().registerCallback(callbackManager,
-//            object : FacebookCallback<LoginResult> {
-//                override fun onSuccess(result: LoginResult) {
-//                    Toast.makeText(
-//                        this@AuthenticationActivity,
-//                        "Login with Facebook success",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//
-//                override fun onCancel() {
-//                    // App code
-//                }
-//
-//                override fun onError(error: FacebookException) {
-//                    Toast.makeText(
-//                        this@AuthenticationActivity,
-//                        "Login with Facebook fail: ${error.message}",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                    Log.d("Login", "${error.message}")
-//                }
-//            })
-//    }
-//
-//    private fun initGoogleSSO() {
-//        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//            .requestEmail()
-//            .build()
-//
-//        googleSignInClient = GoogleSignIn.getClient(this, gso)
-//    }
+    private fun initGoogleSSO() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
 
-//    private fun googleSignIn() {
-//        val signInIntent = googleSignInClient.signInIntent
-//
-//        googleLoginForResult.launch(signInIntent)
-//    }
-//
-//    private fun facebookSignIn() {
-//        LoginManager.getInstance().logInWithReadPermissions(
-//            this,
-//            callbackManager,
-//            listOf("public_profile")
-//        )
-//    }
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+    }
+
+    private fun googleSignIn() {
+        val signInIntent = googleSignInClient.signInIntent
+
+        googleLoginForResult.launch(signInIntent)
+    }
+
+    private fun initFacebookSSO() {
+        callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    toast("Login with Facebook success")
+                }
+
+                override fun onCancel() {
+                    // App code
+                }
+
+                override fun onError(error: FacebookException) {
+                    toast("Login with Facebook fail: ${error.message}")
+                    Log.d("Login", "${error.message}")
+                }
+            })
+    }
+
+
+    private fun facebookSignIn() {
+        LoginManager.getInstance().logInWithReadPermissions(
+            this,
+            callbackManager,
+            listOf("public_profile")
+        )
+    }
     override fun <U> getObjectResponse(data: U) {
         super.getObjectResponse(data)
         if(data is LoginResponse) toast(data.accessToken ?: "")
